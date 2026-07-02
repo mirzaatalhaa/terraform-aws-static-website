e23aaf4826a60dcefeb791fd75fe2f72beb5b11e
mirzaatalhaa <144937151+mirzaatalhaa@users.noreply.github.com>
2026-07-02 20:27:57 +0530

    feat: add custom domain support

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -7,4 +7,8 @@ module "website" {
 
   bucket_name  = "my-react-site-demo"
   project_name = "React Demo"
+  aliases = [
+    "example.com"
+  ]
+  certificate_arn = "arn:aws:acm:us-east-1:111111111111:certificate/12345678-1234-1234-1234-123456789012" 
 }
